ef74e2c44f6d26a8e73fea8219a5c5b16ba4a0b3
ayishahudakt <24mcag01@meaec.edu.in>
2024-10-23 11:41:30 +0530

    Delete pgm10.py

--- pgm10.py
+++
@@ -1,8 +0,0 @@
-user_input=input("enter alist of integers seprated by spaces:")
-result=[]
-for num in user_input.split():
-    if int (num)>100:
-        result.append('over')
-    else:
-        result.append(int(num))
-print("modified list",result)
