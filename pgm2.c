d4cc86f2f78ef640799b4662973e414cedaaa579
ayishahudakt <24mcag01@meaec.edu.in>
2025-01-03 02:17:33 +0530

    Array merging

--- pgm2.c
+++ pgm2.c
@@ -7,44 +7,7 @@ int main()
         printf("\n enter the size of 2nd array:");
         scanf("%d",&s2);
         s3=s1+s2;
-        h()
-{
-        int val;
-        if(top==n)
-        {
-                printf("\n OVERFLOW");
-        }
-        else
-        {
-                printf("\n enter the value:");
-                scanf("%d",&val);
-                top=top+1;
-                stack[top]=val;
-        }
-}
-void pop()
-{
-        if(top==-1)
-        {
-                printf("UNDERFLOW");
-        }
-        else
-        {
-                top=top-1;
-        }
-}
-void display()
-{
-        for(i=top;i>=0;i--)
-        {
-                printf("%d",stack[i]);
-        }
-        if(top==-1)
-        {
-                printf("stack is empty");
-        }
-}
-nt k=s1,j,i,a1[s1],a2[s2],a3[s3];
+        int k=s1,j,i,a1[s1],a2[s2],a3[s3];
         printf("enter the elements in 1st array:");
         for(i=0;i<s1;i++)
         {
@@ -53,13 +16,13 @@ nt k=s1,j,i,a1[s1],a2[s2],a3[s3];
         }
         printf("\n enter the elements on 2nd array:");
         for(i=0;i<s2;i++)
-      {
+        {
                 scanf("%d",&a2[i]);
                 a3[k]=a2[i];
                 k++;
         }
         printf("the merged array before sorting:\n\t");
-       for(i=0;i<s3;i++)
+        for(i=0;i<s3;i++)
         {
                 printf("%d",a3[i]);
         }
